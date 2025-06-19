07291e8af7f605beb6f25c589bf023f4d91b079d
viperud <umhatre1@binghamton.edu>
2025-06-19 13:11:40 -0500

    Step 10: Creating left, right and sorted index variables

--- scientific-computing-with-python/learn-data-structures-by-building-the-merge-sort-algorithm/main.py
+++ scientific-computing-with-python/learn-data-structures-by-building-the-merge-sort-algorithm/main.py
@@ -4,3 +4,7 @@ def merge_sort(array):
     right_part = array[middle_point:]
     merge_sort(left_part)
     merge_sort(right_part)
+
+    left_array_index = 0
+    right_array_index = 0
+    sorted_index = 0
