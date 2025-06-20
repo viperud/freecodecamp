37729844ddce1b7b954a351ce0a47ef257d0eceb
viperud <umhatre1@binghamton.edu>
2025-06-20 12:28:05 -0500

    Step 33: Printing sorted array

--- scientific-computing-with-python/learn-data-structures-by-building-the-merge-sort-algorithm/main.py
+++ scientific-computing-with-python/learn-data-structures-by-building-the-merge-sort-algorithm/main.py
@@ -33,6 +33,7 @@ def merge_sort(array):
 
 if __name__ == '__main__':
     numbers = [4, 10, 6, 14, 2, 1, 8, 5]
-    print('Unsorted array:')
+    print('Unsorted array: ')
     print(numbers)
     merge_sort(numbers)
+    print('Sorted array: ', str(numbers))
