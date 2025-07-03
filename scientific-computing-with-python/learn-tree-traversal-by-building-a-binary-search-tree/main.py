52b39eda3be6fa1726827f9118bff90696b75923
viperud <umhatre1@binghamton.edu>
2025-07-03 15:34:32 -0500

    Step 15: Return node after insertion

--- scientific-computing-with-python/learn-tree-traversal-by-building-a-binary-search-tree/main.py
+++ scientific-computing-with-python/learn-tree-traversal-by-building-a-binary-search-tree/main.py
@@ -16,3 +16,5 @@ class BinarySearchTree:
             node.left = self._insert(node.left, key)
         elif key > node.key:
             node.right = self._insert(node.right, key)
+
+        return node
