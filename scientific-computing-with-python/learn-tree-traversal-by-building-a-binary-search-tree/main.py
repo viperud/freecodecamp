cfcddffd2b8b81e429b22c6dbe33597096c76f7c
viperud <umhatre1@binghamton.edu>
2025-07-03 14:41:20 -0500

    Step 13: Checking if key is greater than current node key

--- scientific-computing-with-python/learn-tree-traversal-by-building-a-binary-search-tree/main.py
+++ scientific-computing-with-python/learn-tree-traversal-by-building-a-binary-search-tree/main.py
@@ -11,5 +11,8 @@ class BinarySearchTree:
     def _insert(self, node, key):
         if node is None:
             TreeNode(key)
+        
         if key < node.key:
             node.left = self._insert(node.left, key)
+        elif key > node.key:
+            pass
