316e2678f7da6d7e7fdace90ae06e0db5c16e928
viperud <umhatre1@binghamton.edu>
2025-07-22 18:10:04 -0500

    Step 51: Return value of all function on the list of valid_in_row, valid_in_col and valid_in_square

--- scientific-computing-with-python/learn-classes-and-objects-by-building-a-sudoku-solver/main.py
+++ scientific-computing-with-python/learn-classes-and-objects-by-building-a-sudoku-solver/main.py
@@ -33,6 +33,7 @@ class Board:
         valid_in_row = self.valid_in_row(row, num)
         valid_in_col = self.valid_in_col(col, num)
         valid_in_square = self.valid_in_square(row, col, num)
+        return all([valid_in_row, valid_in_col, valid_in_square])
 
 
 puzzle = [
