0df8012534430c7b2496ea977ce0d7c04a5a1e65
viperud <umhatre1@binghamton.edu>
2025-07-24 14:37:12 -0500

    Step 65: Print solved sudoku board if solver method returns true

--- scientific-computing-with-python/learn-classes-and-objects-by-building-a-sudoku-solver/main.py
+++ scientific-computing-with-python/learn-classes-and-objects-by-building-a-sudoku-solver/main.py
@@ -50,6 +50,8 @@ class Board:
 def solve_sudoku(board):
     gameboard = Board(board)
     print(f'Puzzle to solve:\n{gameboard}')
+    if gameboard.solver():
+        print(f'Solved puzzle:\n{gameboard}')
 
 puzzle = [
   [0, 0, 2, 0, 0, 8, 0, 0, 0],
