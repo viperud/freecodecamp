5eb6267b384f77ded9c483341363b086625a9635
viperud <umhatre1@binghamton.edu>
2025-07-24 13:51:30 -0500

    Step 58: Assign guess value to current tupple in board

--- scientific-computing-with-python/learn-classes-and-objects-by-building-a-sudoku-solver/main.py
+++ scientific-computing-with-python/learn-classes-and-objects-by-building-a-sudoku-solver/main.py
@@ -41,6 +41,7 @@ class Board:
         for guess in range(1, 10):
             if self.is_valid(next_empty, guess):
                 row, col = next_empty
+                self.board[row][col] = guess
 
 
 puzzle = [
