1db73e5c591c3154f1b24864accf62d9efabcd87
viperud <umhatre1@binghamton.edu>
2025-10-02 22:59:11 -0500

    Code with 3 test cases passing

--- data-analysis-with-python/demographic-data-analyzer/demographic_data_analyzer.py
+++ data-analysis-with-python/demographic-data-analyzer/demographic_data_analyzer.py
@@ -2,16 +2,19 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
+    #print(df[df['education'] == 'Bachelors']['race'].count())
+    #print(df['race'].count())
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df[df['sex'] == 'Male']['age'].mean().round(2)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    no_of_bach = df[df['education'] == 'Bachelors']['race'].count()
+    percentage_bachelors = ((no_of_bach * 100) / df['race'].count()).round(2)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
@@ -66,3 +69,5 @@ def calculate_demographic_data(print_data=True):
         highest_earning_country_percentage,
         'top_IN_occupation': top_IN_occupation
     }
+
+calculate_demographic_data(True)
