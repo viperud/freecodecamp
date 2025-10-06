f65d3e8e1b2a420a605d112fc1effcb971ba278d
viperud <umhatre1@binghamton.edu>
2025-10-06 16:10:52 -0500

    Code with all test cases passing

--- data-analysis-with-python/medical-data-visualizer/medical_data_visualizer.py
+++ data-analysis-with-python/medical-data-visualizer/medical_data_visualizer.py
@@ -5,31 +5,36 @@ import numpy as np
 
 # 1
 df = pd.read_csv('medical_examination.csv')
-print(df)
 
 # 2
-df['overweight'] = None
+BMI = df['weight'] / (df['height'] / 100)**2
+BMI = [1 if bbmi > 25 else 0 for bbmi in BMI]
+df['overweight'] = BMI
 
 # 3
-
+#df.loc[df['cholesterol'] == 1, 'cholesterol'] = 0
+#df.loc[df['cholesterol'] > 1, 'cholesterol'] = 1
+#df.loc[df['gluc'] == 1, 'gluc'] = 0
+#df.loc[df['gluc'] > 1, 'gluc'] = 1
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'],
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], 
+                     value_name = "value")    
 
     # 6
-    df_cat = None
+    df_cat["total"] = 1
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index = False).count()
     
-
     # 7
-
-
+    catplot = sns.catplot(data=df_cat, x='variable', y='total', col='cardio', kind='bar', hue='value')
 
     # 8
-    fig = None
-
+    fig = catplot.fig
 
     # 9
     fig.savefig('catplot.png')
@@ -39,21 +44,35 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) & 
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
+    mask = np.triu(corr)
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # 15
-
+    sns.heatmap(data=corr, 
+                annot=True, 
+                fmt=".1f", 
+                linewidth=.5, 
+                mask=mask, 
+                annot_kws={'fontsize':6}, 
+                cbar_kws={"shrink": .7}, 
+                square=False, 
+                center=0, 
+                vmax=0.30)
 
 
     # 16
