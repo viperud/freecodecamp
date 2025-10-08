1b4fab2ddc2804decf996023768206834b13718b
viperud <umhatre1@binghamton.edu>
2025-10-08 13:13:26 -0500

    Code with all test cases passing

--- data-analysis-with-python/sea-level-predictor/sea_level_predictor.py
+++ data-analysis-with-python/sea-level-predictor/sea_level_predictor.py
@@ -6,18 +6,26 @@ def draw_plot():
     # Read data from file
     df = pd.read_csv('epa-sea-level.csv')
 
-
     # Create scatter plot
-
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
     # Create first line of best fit
-
+    result = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x_range = range(df['Year'].min(), 2051, 1)
+    y_range = [result.slope * yr + result.intercept for yr in x_range]
+    plt.plot(x_range, y_range)
 
     # Create second line of best fit
-
+    df_c = df[df['Year'] >= 2000]
+    result = linregress(df_c['Year'], df_c['CSIRO Adjusted Sea Level'])
+    x_range = range(df_c['Year'].min(), 2051, 1)
+    y_range = [result.slope * yr + result.intercept for yr in x_range]
+    plt.plot(x_range, y_range)
 
     # Add labels and title
-
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
+    plt.title("Rise in Sea Level")
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
